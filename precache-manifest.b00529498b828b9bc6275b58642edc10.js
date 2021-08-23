55adefa74bc73cb09f251cbf09690123b72ce903
Tonymndz <Tonymndz1141@gmail.com>
2021-08-23 14:51:15 -0700

    Updates

---
+++ precache-manifest.b00529498b828b9bc6275b58642edc10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b9d58439f73e402936723d228d9d68",
+    "url": "/Budget-Phones/index.html"
+  },
+  {
+    "revision": "03f78c6a697b34b5b088",
+    "url": "/Budget-Phones/static/css/main.02fc762c.chunk.css"
+  },
+  {
+    "revision": "6055091d3f937f99e518",
+    "url": "/Budget-Phones/static/js/2.a27537d4.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/Budget-Phones/static/js/2.a27537d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f78c6a697b34b5b088",
+    "url": "/Budget-Phones/static/js/main.9df8ef07.chunk.js"
+  },
+  {
+    "revision": "7f7ddb0f2ee4a1d41b71",
+    "url": "/Budget-Phones/static/js/runtime-main.32480993.js"
+  }
+]);
